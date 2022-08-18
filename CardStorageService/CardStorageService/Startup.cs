ce937160800af1785aa5ae0ac81975d922f5c683
HunnidStax <den4ik.trofimenko@gmail.com>
2022-08-18 23:22:25 +0300

    BaseForAuthentication

--- CardStorageService/CardStorageService/Startup.cs
+++ CardStorageService/CardStorageService/Startup.cs
@@ -24,6 +24,7 @@ namespace CardStorageService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IAuthenticateService, AuthenticateService>();
 
             services.AddScoped<IClientRepositoryService, ClientRepository>();
             services.AddScoped<ICardRepositoryService, CardRepository>();
